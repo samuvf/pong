22c96d991a357f923380e732415df5972e2bb90a
samuvf <samuelvf9@hotmail.com>
2025-12-06 16:39:43 -0300

    feat: add movement to paddles and game states(play, start)

--- Paddle.lua
+++ Paddle.lua
@@ -1,11 +1,21 @@
 Paddle = Class{}
 
-function Paddle:init(x, y, width, height, dy)
+function Paddle:init(x, y, width, height)
   self.x = x
   self.y = y
   self.width = width
   self.height = height
-  self.dy = dy
+  self.dy = 0 -- responsible for the velocity and directiion of the paddle
+end
+
+function Paddle:update(dt)
+  -- paddle can't pass the top
+  if self.dy < 0 then
+    self.y = math.max(0, self.y + self.dy * dt)
+  -- nor the botton of the screen
+  else
+    self.y = math.min(VIRTUAL_HEIGHT - self.height, self.y + self.dy  * dt)
+  end
 end
 
 function Paddle:render()
--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 232
 
+PADDLE_SPEED = 200
+
 Class = require 'class'
 
 require 'Paddle'
@@ -16,9 +18,8 @@ function love.load()
 
   love.graphics.setDefaultFilter('nearest', 'nearest') -- filter for retro game
 
-  smallFont = love.graphics.newFont('font.ttf', 8) -- better font for 2d game
-  
-  love.graphics.setFont(smallFont) 
+  font = love.graphics.newFont('font.ttf', 16) -- better font for 2d game
+  love.graphics.setFont(font)
 
   -- set virtual window for better and consistant resolution whatever the window size is
   push:setupScreen( VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -31,31 +32,69 @@ function love.load()
     return push:resize(w, h)
   end
 
-  player1paddle = Paddle(10, VIRTUAL_HEIGHT / 3, 5, 20)
+  player1paddle = Paddle(5, VIRTUAL_HEIGHT / 3, 5, 20)
   player2paddle = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT / 1.5, 5, 20)
 
   ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 + 2, 4, 4)
+
+  gameState = 'start'
 end
 
 function love.update(dt) 
-  function love.keypressed(key)
-    if key == 'escape' then
-      love.event.quit()
+  -- player 1
+  if love.keyboard.isDown('w') then
+    player1paddle.dy = -PADDLE_SPEED
+  elseif love.keyboard.isDown('s') then
+    player1paddle.dy = PADDLE_SPEED 
+  else
+    player1paddle.dy = 0 -- if no keyboard pressed paddle stays in its place
+  end
+  -- player 2
+  if love.keyboard.isDown('up') then
+    player2paddle.dy = -PADDLE_SPEED 
+  elseif love.keyboard.isDown('down') then
+    player2paddle.dy = PADDLE_SPEED 
+  else
+    player2paddle.dy = 0 -- if no keyboard pressed paddle stays in its place
+  end
+
+  player1paddle:update(dt)
+  player2paddle:update(dt)    
+end
+
+function love.keypressed(key)
+  if key == 'escape' then
+    love.event.quit()
+  elseif key == 'enter' or key == 'return' then
+    if gameState == 'start' then
+      gameState = 'play'
+    else
+      gameState = 'start'
     end
   end
 end
 
 function love.draw() 
   push:start()
-    
-  love.graphics.printf(
-    'Welcome to Pong!',
-    0,                      -- starting x
-    VIRTUAL_HEIGHT / 3 - 6,  -- starting y
-    VIRTUAL_WIDTH,           -- limit
-    'center'                -- alignment
-  )
-
+  
+  if gameState == 'start' then
+    love.graphics.printf(
+      'Press enter to begin',
+      0,                        -- starting x
+      VIRTUAL_HEIGHT / 4 - 10,  -- starting y
+      VIRTUAL_WIDTH,            -- limit
+      'center'                  -- alignment
+    )
+  else
+    love.graphics.printf(
+      'The game has begun',
+      0,                     
+      VIRTUAL_HEIGHT / 4 - 10,  
+      VIRTUAL_WIDTH,           
+      'center'               
+    )
+  end
+  
   player1paddle:render()
   player2paddle:render()
 
@@ -63,3 +102,4 @@ function love.draw()
 
   push:finish()
 end
+
