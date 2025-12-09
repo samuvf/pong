d6a100c8f7a2ef0e74ded93180fc3f8d685804ab
samuvf <samuelvf9@hotmail.com>
2025-12-09 09:17:01 -0300

    feat: score mechanic

--- Ball.lua
+++ Ball.lua
@@ -12,20 +12,19 @@ end
 function Ball:update(dt)
   self.x = self.x + self.dx * dt
   self.y = self.y + self.dy * dt
-  -- collision with top and botton 
-  if self.y <= self.height or self.y >= VIRTUAL_HEIGHT - self.height then 
-    self.dy = -self.dy -- change direction
-  end
 end
 
 function Ball:collision(player)
   -- AABB collision
+  -- if ball is to the left or to the right of the paddle there is no contact
   if ball.x > player.x + player.width or ball.x + ball.width < player.x then
     return false
   end
+  -- if ball is above or under paddle there is no contact
   if ball.y > player.y + player.height or ball.y + ball.height < player.y then
     return false
   end
+  -- else return true for collision
   return true
 end
 
--- main.lua
+++ main.lua
@@ -18,8 +18,9 @@ function love.load()
 
   love.graphics.setDefaultFilter('nearest', 'nearest') -- filter for retro game
 
-  font = love.graphics.newFont('font.ttf', 16) -- better font for 2d game
-  love.graphics.setFont(font)
+  smallFont = love.graphics.newFont('font.ttf', 16) -- better font for 2d game
+
+  bigFont = love.graphics.newFont('font.ttf', 32) 
 
   math.randomseed(os.time())
 
@@ -34,6 +35,9 @@ function love.load()
     return push:resize(w, h)
   end
 
+  scorePlayer1 = 0
+  scorePlayer2 = 0
+
   player1paddle = Paddle(5, VIRTUAL_HEIGHT / 3, 5, 20)
   player2paddle = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT / 1.5, 5, 20)
 
@@ -46,17 +50,47 @@ function love.load()
 end
 
 function love.update(dt) 
+  -- ball collision with paddle1
   if ball:collision(player1paddle) then
-    ball.dx = -ball.dx * 1.03
-    ball.x = ball.x + player1paddle.width
-  end
+    ball.dx = -ball.dx * 1.03             -- change direction and increases velocity
+    ball.x = ball.x + player1paddle.width -- moves ball away from paddle to avoid a collision loop
 
+    -- does not change y direction just its velocity
+    if ball.dy < 0 then
+      ball.dy = -math.random(10,150)
+    else
+      ball.dy = math.random(10,150)
+    end
+  end
+  -- ball collision with paddle2
   if ball:collision(player2paddle) then
     ball.dx = -ball.dx * 1.03
     ball.x = ball.x - ball.width
+
+    if ball.y < 0 then
+      ball.dy = -math.random(10,150)
+    else
+      ball.dy = math.random(10,150)
+    end
+  end
+  -- ball collision with top and botton 
+  if ball.y <= ball.height or ball.y >= VIRTUAL_HEIGHT - ball.height then 
+    ball.dy = -ball.dy -- change direction
   end
 
-  -- player 1
+  -- scores
+  if ball.x < 0 then
+    scorePlayer1 = scorePlayer1 + 1
+
+    gameState = 'start'
+    ball:reset()
+  elseif ball.x > VIRTUAL_WIDTH then
+    scorePlayer2 = scorePlayer2 + 1
+
+    gameState = 'start'
+    ball:reset()
+  end  
+  -- player 1 movement
   if love.keyboard.isDown('w') then
     player1paddle.dy = -PADDLE_SPEED
   elseif love.keyboard.isDown('s') then
@@ -64,7 +98,7 @@ function love.update(dt)
   else
     player1paddle.dy = 0 -- if no keyboard pressed paddle stays in its place
   end
-  -- player 2
+  -- player 2 movement
   if love.keyboard.isDown('up') then
     player2paddle.dy = -PADDLE_SPEED 
   elseif love.keyboard.isDown('down') then
@@ -98,24 +132,29 @@ end
 function love.draw() 
   push:start()
   
+  love.graphics.setFont(smallFont)
   if gameState == 'start' then
     love.graphics.printf(
-      'Press enter to begin',
+      'Game Start State',
       0,                        -- starting x
-      VIRTUAL_HEIGHT / 4 - 10,  -- starting y
+      VIRTUAL_HEIGHT / 5,  -- starting y
       VIRTUAL_WIDTH,            -- limit
       'center'                  -- alignment
     )
   else
     love.graphics.printf(
-      'The game has begun',
+      'Game Play State',
       0,                     
-      VIRTUAL_HEIGHT / 4 - 10,  
+      VIRTUAL_HEIGHT / 5,  
       VIRTUAL_WIDTH,           
       'center'               
     )
   end
-  
+
+  love.graphics.setFont(bigFont)
+  love.graphics.printf(scorePlayer1, 150, VIRTUAL_HEIGHT / 3 - 10, VIRTUAL_WIDTH, 'left')
+  love.graphics.printf(scorePlayer2, 0, VIRTUAL_HEIGHT / 3 - 10, VIRTUAL_WIDTH - 150, 'right')
+
   player1paddle:render()
   player2paddle:render()
 
